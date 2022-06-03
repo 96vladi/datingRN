abf4351ddaf169560668c572ab60fe3082c3c592
Vladimir Maidana <38408590+96vladi@users.noreply.github.com>
2022-06-03 16:40:13 -0400

    starting-project

---
+++ src/components/Generals/index.js
@@ -0,0 +1,8 @@
+import styled from 'styled-components/native';
+
+export const CustomText = styled.Text`
+  margin-top: 20px;
+  font-size: 24px;
+  font-weight: bold;
+  text-align: center;
+`;
---
+++ src/screens/Home/index.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import { CustomText } from '../../components/Generals'
+
+const Home = () => {
+  return (
+    <CustomText>Administrador De Citas</CustomText>
+  )
+}
+
+export default Home
