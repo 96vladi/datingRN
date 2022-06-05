a81a5fd798efba460f15ec51ba1332cce9994f18
Vladimir Maidana <38408590+96vladi@users.noreply.github.com>
2022-06-05 15:39:19 -0400

    create-button-delet

--- src/components/LIst/index.js
+++ src/components/LIst/index.js
@@ -1,22 +1,28 @@
 import React from 'react'
 import { CustomText } from '../Generals';
-import { ContainerList, ContainerText } from './styledComponent';
+import { BtnDelet, ContainerList, ContainerText } from './styledComponent';
 
 const List = ({cita}) => {
+  const dialogoEliminar = () => {
+    console.log("Eliminando...")
+  };
   return (
     <ContainerList>
       <ContainerText>
-        <CustomText clr='black' fz='18px' mt='20px'>Paciente: </CustomText>
-        <CustomText clr='black' fz='18px' fw='normal'>{cita.paciente}</CustomText>
+        <CustomText clr='black' fz='18px' mt='20px' align='left'>Paciente: </CustomText>
+        <CustomText clr='black' fz='18px' fw='normal' align='left'>{cita.paciente}</CustomText>
       </ContainerText>
       <ContainerText>
-        <CustomText clr='black' fz='18px' mt='20px'>Propietario: </CustomText>
-        <CustomText clr='black' fz='18px' fw='normal'>{cita.propietario}</CustomText>
+        <CustomText clr='black' fz='18px' mt='20px' align='left'>Propietario: </CustomText>
+        <CustomText clr='black' fz='18px' fw='normal' align='left'>{cita.propietario}</CustomText>
       </ContainerText>
       <ContainerText>
-        <CustomText clr='black' fz='18px' mt='20px'>Sintomas: </CustomText>
-        <CustomText clr='black' fz='18px' fw='normal'>{cita.sintomas}</CustomText>
+        <CustomText clr='black' fz='18px' mt='20px' align='left'>Sintomas: </CustomText>
+        <CustomText clr='black' fz='18px' fw='normal' align='left'>{cita.sintomas}</CustomText>
       </ContainerText>
+      <BtnDelet onPress={ () => dialogoEliminar()}>
+        <CustomText>Eliminar &times;</CustomText>
+      </BtnDelet>
     </ContainerList>
   )
 }
--- src/components/LIst/styledComponent.js
+++ src/components/LIst/styledComponent.js
@@ -5,8 +5,14 @@ export const ContainerList = styled.View`
   border-bottom-color: #e1e1e1;
   border-style: solid;
   border-bottom-width: 1px;
-  padding: 20px 10px 20px 10px;
+  padding: 0px 10px 0px 10px;
 `;
 export const ContainerText = styled.View`
   
+`;
+
+export const BtnDelet = styled.TouchableHighlight`
+  padding: 10px;
+  background-color: red;
+  margin: 10px 0px 10px 0px;
 `;
