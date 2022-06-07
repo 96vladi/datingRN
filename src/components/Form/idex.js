fd478cf1bfcf16da1f2317ddb17e258450c8f786
Vladimir Maidana <38408590+96vladi@users.noreply.github.com>
2022-06-06 20:06:45 -0400

    date-install

--- src/components/Form/idex.js
+++ src/components/Form/idex.js
@@ -1,49 +1,83 @@
-import React, { useState } from 'react'
-import { CustomText } from '../Generals'
-import { ContainerForm, ContainerInput, CustomInput } from './styledComponent'
+import React, {useState} from 'react';
+import {CustomText} from '../Generals';
+import {ContainerForm, ContainerInput, CustomInput} from './styledComponent';
+import { View, Button } from 'react-native';
+import DateTimePickerModal from 'react-native-modal-datetime-picker';
 
 const Form = () => {
+  const [isDatePickerVisible, setDatePickerVisibility] = useState(false);
+
+  const showDatePicker = () => {
+    setDatePickerVisibility(true);
+  };
+
+  const hideDatePicker = () => {
+    setDatePickerVisibility(false);
+  };
+
+  const handleConfirm = date => {
+    console.warn('A date has been picked: ', date);
+    hideDatePicker();
+  };
+
   return (
     <>
       <ContainerForm>
         <ContainerInput>
-          <CustomText fz='18px' mt='20px' clr='black' align='left'>Paciente</CustomText>
-          <CustomInput 
-            onChangeText = { (text) => {
+          <CustomText fz="18px" mt="20px" clr="black" align="left">
+            Paciente
+          </CustomText>
+          <CustomInput
+            onChangeText={text => {
               console.log(text);
             }}
           />
         </ContainerInput>
         <ContainerInput>
-          <CustomText fz='18px' mt='20px' clr='black' align='left'>Dueño</CustomText>
-          <CustomInput 
-            onChangeText = { (text) => {
+          <CustomText fz="18px" mt="20px" clr="black" align="left">
+            Dueño
+          </CustomText>
+          <CustomInput
+            onChangeText={text => {
               console.log(text);
             }}
           />
         </ContainerInput>
         <ContainerInput>
-          <CustomText fz='18px' mt='20px' clr='black' align='left'>Contacto</CustomText>
-          <CustomInput 
-            onChangeText = { (text) => {
+          <CustomText fz="18px" mt="20px" clr="black" align="left">
+            Contacto
+          </CustomText>
+          <CustomInput
+            onChangeText={text => {
               console.log(text);
             }}
-            keyboardType= 'numeric'
+            keyboardType="numeric"
           />
         </ContainerInput>
         <ContainerInput>
-          <CustomText fz='18px' mt='20px' clr='black' align='left'>Sintomas</CustomText>
-          <CustomInput 
+          <CustomText fz="18px" mt="20px" clr="black" align="left">
+            Sintomas
+          </CustomText>
+          <CustomInput
             multiline
-            onChangeText = { (text) => {
+            onChangeText={text => {
               console.log(text);
             }}
-            keyboardType= 'numeric'
+            keyboardType="numeric"
           />
         </ContainerInput>
+        <View>
+          <Button title="Show Date Picker" onPress={showDatePicker} />
+          <DateTimePickerModal
+            isVisible={isDatePickerVisible}
+            mode="date"
+            onConfirm={handleConfirm}
+            onCancel={hideDatePicker}
+          />
+        </View>
       </ContainerForm>
     </>
-  )
-}
+  );
+};
 
-export default Form
+export default Form;
